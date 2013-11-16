2fea15a9c53316d2f3dda0cb6c2aa8800068f5ff
triktron <ele.trik@hotmail.com>
2013-11-16 19:49:09 +0100

    fixd jumb 1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var ak_lr = 3;
 var ak_ud = 3;
-var jump_time = 3;
+var jump_time = 4;
 
 function main() {
 	if (ak_lr == 1 && parseInt(document.getElementById('turtel').style.left) > 50) {
@@ -13,8 +13,9 @@ function main() {
 		jump1();
 	} else if (jump_time == 2) {
 		jump2();
+	} else if (jump_time == 3) {
+		jump3();
 	}
-	
 	
 	setTimeout(function(){main();},1000/30);
 }
@@ -42,17 +43,24 @@ function move(e)
 }
 
 function jump1() {
-	if (parseInt(document.getElementById('turtel').style.top) > 200 ) {
+	if (parseInt(document.getElementById('turtel').style.top) > 270 ) {
 		document.getElementById('turtel').style.top = (parseInt(document.getElementById('turtel').style.top) - 4) + "px";
 	} else {
 		jump_time = 2;
 	}
 }
 function jump2() {
+	if (parseInt(document.getElementById('turtel').style.top) > 230 ) {
+		document.getElementById('turtel').style.top = (parseInt(document.getElementById('turtel').style.top) - 2) + "px";
+	} else {
+		jump_time = 3;
+	}
+}
+function jump3() {
 	if (parseInt(document.getElementById('turtel').style.top) != 340 ) {
 		document.getElementById('turtel').style.top = (parseInt(document.getElementById('turtel').style.top) + 4) + "px";
 	} else {
-		jump_time = 3;
+		jump_time = 4;
 	}
 }
 
